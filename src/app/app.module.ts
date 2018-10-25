be59a251e58148d6b558ebb3428fa48b438a0f4f
Romualdo Dasig <dasig.rg@a5project.com>
2018-10-25 16:17:48 +0800

    Tour of Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
+import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   imports: [
@@ -37,7 +37,6 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     MessagesComponent,
     HeroSearchComponent
   ],
-  providers: [],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
